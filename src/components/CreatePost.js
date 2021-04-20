c818009730ce575987339a3db34e65c3a69d040e
gmohseni <47803833+gmohseni@users.noreply.github.com>
2021-04-20 13:43:11 -0700

    check URL/Body

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -4,13 +4,16 @@ import { useDispatch, useSelector } from 'react-redux';
 import { createPost, updatePost } from '../actions/posts';
 import { useParams } from "react-router-dom";
 
+// let errorFlag = false;
 
 const CreatePost  = () => {
     const [postData, setPostData] = useState({title: '', url: '', message: ''});
+    const [errorFlag, setErrorFlag] = useState(false);
     var {id} = useParams();
     const post = useSelector((state) => id ? state.posts.find((p) => p._id === id) : null);
     const dispatch = useDispatch();
     const user = JSON.parse(localStorage.getItem('profile'));
+    
 
     useEffect(() => {
         if (id === undefined){
@@ -21,19 +24,59 @@ const CreatePost  = () => {
         }
     }, [id, post]);
 
+    const checkErrorFlag = () => {
+        if ((postData.url.length > 0) && (postData.message.length > 0)){
+            setErrorFlag(true);
+            // console.log(errorFlag);
+        }
+        else if((postData.url.length === 0) && (postData.message.length === 0)){
+           setErrorFlag(true);
+        }
+        // console.log(errorFlag);
+
+    }
+
+const checkURL = (url) => {
+    if (postData.message.length > 0){
+        setErrorFlag(true);
+    }
+    else{
+        setPostData({ ...postData, url: url });
+    }
+}
+
+const checkText = (message) => {
+    if (postData.url.length > 0){
+        setErrorFlag(true);
+    }
+    else{
+        setPostData({ ...postData, message: message });
+    }
+}
+
+
+
     const handleSubmit = (e) => {
-        if (id !== undefined){
-            dispatch(updatePost(id, postData));
-        }
-        else if (id === undefined){
-            dispatch(createPost({...postData,username: user?.result?.username}));
-        }
+
+         
     
-        clear();
+
+        
+
+            if (id !== undefined){
+                dispatch(updatePost(id, postData));
+            }
+            else if (id === undefined){
+                dispatch(createPost({...postData,username: user?.result?.username}));
+            }
+
+        
+         clear();
     }
 
     const clear = () => {
         setPostData({title: '', url:'', message: ''});
+        setErrorFlag(false);
     }
 
    
@@ -52,11 +95,26 @@ const CreatePost  = () => {
                 <div className ="col-sm-1"></div>
             </div>
             <div className="row">
+            <div className="col-sm-12">
+            
+            {
+                   
+                    (errorFlag) ?
+                    <div className="alert alert-danger">Can only have a URL or Text, not both!</div>
+                    : 
+                    <>
+                    </>
+                    
+                    }
+            </div>
+            </div>
+
+            <div className="row">
             <div className ="col-sm-1">
                 </div>
                 <div className="col-sm-10">
                     <label htmlFor="Title">Title</label>
-                    <input id="Title" type="text" placeholder="Title" value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })}></input>
+                    <input id="Title" required type="text" placeholder="Title" value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })}></input>
                 </div>
                 <div className ="col-sm-1"></div>
             </div>
@@ -65,7 +123,7 @@ const CreatePost  = () => {
                 </div>
                 <div className="col-sm-10">
                     <label htmlFor="URL">URL</label>
-                    <textarea id="URL" type="text" placeholder="URL" cols="80" value={postData.url} onChange={(e) => setPostData({ ...postData, url: e.target.value })}></textarea>
+                    <textarea id="URL" type="text" placeholder="URL" cols="80" value={postData.url} onChange={(e) => checkURL(e.target.value)}></textarea>
                 </div>
                 <div className ="col-sm-1"></div>
             </div>
@@ -74,14 +132,14 @@ const CreatePost  = () => {
                 </div>
                 <div className="col-sm-10">
                     <label htmlFor="Body">Body of Post</label>
-                    <textarea id="Body" type="text" placeholder="Body" cols="100" rows="5" value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })}></textarea>
+                    <textarea id="Body" type="text" placeholder="Body" cols="100" rows="5" value={postData.message} onChange={(e) => checkText(e.target.value)}></textarea>
                 </div>
                 <div className ="col-sm-1"></div>
             </div>
             <div className="row">
             <div className ="col-sm-1"></div>
             <div className="col-sm-10 float-end">
-                    <button onClick={() => handleSubmit()}>Submit</button>
+                    <button disabled={errorFlag} onClick={() => handleSubmit()}>Submit</button>
                     <button onClick={() => clear()}>Clear</button>
             </div>
             <div className ="col-sm-1"></div>
